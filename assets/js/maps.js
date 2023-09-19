0a27445a0a37433b068b0608c958eedde5ca9184
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-09-18 23:05:00 -0500

    added new call

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,14 @@
 function initMap() {
     const directionsRenderer = new google.maps.DirectionsRenderer();
     const directionsService = new google.maps.DirectionsService();
-
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: new google.maps.LatLng("32.707548", "-117.157246"),
+    });
 
 }
+
+directionsRenderer.setMap(map);
 
 
 
