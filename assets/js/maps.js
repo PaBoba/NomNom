abef13f7aa187215dd0d62038769580e45402da5
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-09-18 22:57:08 -0500

    Start creating new function for travel routes as opposed to search map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,12 +1,27 @@
 function initMap() {
-
-    let mapOptions = {
-        center: new google.maps.LatLng("32.707548", "-117.157246"),
-        zoom: 16
-
-    }
-    
-    let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const directionsService = new google.maps.DirectionsService();
 
 
 }
+
+
+
+
+
+
+
+
+
+// function initMap() {
+
+//     let mapOptions = {
+//         center: new google.maps.LatLng("32.707548", "-117.157246"),
+//         zoom: 16
+
+//     }
+    
+//     let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
+// }
