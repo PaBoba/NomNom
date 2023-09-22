f6960fb7b81ba64e448e282701a4e5ce4ee65948
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-09-22 00:59:48 -0500

    added location based button and services

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -9,7 +9,7 @@ function initMap() {
         center: { lat: 32.707548, lng: -117.157246 },
         zoom: 13,
     })
-    google.maps.event.addListener(map, "click", function (event) {
+    google.maps.event.addListener(map, "mouseover", function (event) {
         this.setOptions({ scrollwheel: true });
     })
     directionsService = new google.maps.DirectionsService();
@@ -22,7 +22,54 @@ function initMap() {
     desAutocomplete = new google.maps.places.Autocomplete(
         document.getElementById("dest")
     )
+
+    infoWindow = new google.maps.InfoWindow();
+
+    const locationButton = document.createElement("button");
+
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    // This is where I need to figure out how to remove the button and place it next to the get directions
+    // I am not sure where/ how to pull the button and attach the function like the one above
+    // The control can be moved according to this link https://developers.google.com/maps/documentation/javascript/controls#ControlPositioning
+    map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("Location found.");
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                },
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation.",
+    );
+    infoWindow.open(map);
+}
+
+window.initMap = initMap;
 
 function calcRoute() {
     const source = document.getElementById("source").value
