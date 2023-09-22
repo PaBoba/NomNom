146509f3eba97441c753e38438f92a454b06ce5d
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-09-21 23:44:59 -0500

    removed commented out code in maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -47,22 +47,3 @@ googleSearch.addEventListener("click", function (event) {
     event.preventDefault();
     calcRoute();
 });
-
-
-
-
-
-
-
-// function initMap() {
-
-//     let mapOptions = {
-//         center: new google.maps.LatLng("32.707548", "-117.157246"),
-//         zoom: 16
-
-//     }
-    
-//     let map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-
-// }
