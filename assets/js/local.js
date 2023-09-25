c0bc24f0610ecf304b15a392231a570fe1b5dc85
PaBoba <140555335+PaBoba@users.noreply.github.com>
2023-09-25 15:21:11 -0700

    debug console.log

--- assets/js/local.js
+++ assets/js/local.js
@@ -2,22 +2,25 @@ const restaurants = $("#restaurants");
 const locations = $("#location");
 
 function storeChoice() {
-  let choices = {
-    restaurants: restaurants.value.trim(),
-    locations: locations.value.trim(),
-  };
-  localStorage.setItem('choices', JSON.stringify(choices));
+
+    let choices = {
+        restaurants: restaurants.value.trim(),
+        locations: locations.value.trim(),
+    };
+    localStorage.setItem('choices', JSON.stringify(choices));
 }
 
 function renderLastChoice() {
-  let lastChoice = JSON.parse(localStorage.getItem('choices'));
-  if (lastChoice !== null) {
-  console.log(lastChoice.restaurants);
-  console.log(lastChoice.locations);
-  }
+    let lastChoice = JSON.parse(localStorage.getItem('choices'));
+    if (lastChoice !== null) {
+        console.log(lastChoice.restaurants);
+        console.log(lastChoice.locations);
+    }
 }
 
+storeChoice();
+
 function init() {
-  renderLastChoice();
+    renderLastChoice();
 }
 init();
