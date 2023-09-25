90c1f4248ff473dd8c4907d1f9cdcae52b9051cf
PaBoba <140555335+PaBoba@users.noreply.github.com>
2023-09-25 16:44:08 -0700

    local storage stores most recent entry

--- assets/js/local.js
+++ assets/js/local.js
@@ -1,26 +1,31 @@
-const restaurants = $("#restaurants");
-const locations = $("#location");
+const restaurants = document.getElementById("restaurants");
+const locations = document.getElementById("location");
+const nomButton = document.getElementById("nom");
 
 function storeChoice() {
 
     let choices = {
-        restaurants: restaurants.value.trim(),
-        locations: locations.value.trim(),
+        restaurants: restaurants.value,
+        locations: locations.value,
     };
     localStorage.setItem('choices', JSON.stringify(choices));
 }
 
 function renderLastChoice() {
     let lastChoice = JSON.parse(localStorage.getItem('choices'));
+    console.log(lastChoice);
     if (lastChoice !== null) {
         console.log(lastChoice.restaurants);
         console.log(lastChoice.locations);
     }
 }
-
-storeChoice();
+nomButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    storeChoice();
+    renderLastChoice();
+});
 
 function init() {
-    renderLastChoice();
-}
+        renderLastChoice();
+    }
 init();
