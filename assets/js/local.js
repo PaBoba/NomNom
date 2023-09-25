68f7dbc50f66a73d28ad43b5a8fe25c25098cc08
PaBoba <140555335+PaBoba@users.noreply.github.com>
2023-09-25 15:04:46 -0700

    fixed typos

--- assets/js/local.js
+++ assets/js/local.js
@@ -2,19 +2,15 @@ const restaurants = $("#restaurants");
 const locations = $("#location");
 
 function storeChoice() {
-  // Save related form data as an object
   let choices = {
     restaurants: restaurants.value.trim(),
     locations: locations.value.trim(),
   };
-  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
   localStorage.setItem('choices', JSON.stringify(choices));
 }
 
 function renderLastChoice() {
-  // Use JSON.parse() to convert text to JavaScript object
   let lastChoice = JSON.parse(localStorage.getItem('choices'));
-  // Check if data is returned, if not exit out of the function
   if (lastChoice !== null) {
   console.log(lastChoice.restaurants);
   console.log(lastChoice.locations);
